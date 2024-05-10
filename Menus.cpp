1f4b511adf796a6b78289059ceda853f9bfb1436
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-05-10 22:48:42 +0500

    feat: Lists'/Menus' functions layout

--- Menus.cpp
+++ Menus.cpp
@@ -1,13 +1,97 @@
 #include "Menus.h"
+#include <iostream>
 
-void Menus::bookMenu() {}
+using namespace std;
 
-void Menus::employeesMenu() {}
+void Menus::bookMenu() {
+    int funcChoice;
 
-void Menus::membersMenu() {}
+    cout << "*************************************************\n";
+	cout << "                    Books Menu                   \n";
+	cout << "*************************************************\n";
+	cout << "   1. Add Book\n";
+	cout << "   2. Update Book Price\n";
+	cout << "   3. Search Book\n";
+	cout << "   4. Update Book Status\n";
+	cout << "   5. Display All Books\n";
+	cout << "   6. Return To Main Menu\n\n\n";
+	cout << "Enter Your Choice : \n";
 
-void Menus::purchasesMenu() {}
+    cin >> funcChoice;
+}
 
-void Menus::salesMenu() {}
+void Menus::employeesMenu() {
+    int funcChoice;
 
-void Menus::suppliersMenu() {}
+    cout << "*************************************************\n";
+	cout << "                   Employees List                \n";
+	cout << "*************************************************\n";
+	cout << "   1. New Employee\n";
+	cout << "   2. Search Employee\n";
+	cout << "   3. Assign Manager\n";
+	cout << "   4. View All\n";
+	cout << "   5. Update Salary\n";
+	cout << "   6. Return To Main Menu\n\n\n";
+	cout << "Enter Your Choice : \n";
+
+    cin >> funcChoice;
+}
+
+void Menus::membersMenu() {
+    int funcChoice;
+
+    cout << "*************************************************\n";
+	cout << "                    Members List                 \n";
+	cout << "*************************************************\n";
+	cout << "   1. New Member\n";
+	cout << "   2. Search Member\n";
+	cout << "   3. Return To Main Menu\n\n\n";
+	cout << "Enter Your Choice : \n";
+
+    cin >> funcChoice;
+}
+
+void Menus::purchasesMenu() {
+    int funcChoice;
+
+    cout << "*************************************************\n";
+	cout << "                   Purchases List                \n";
+	cout << "*************************************************\n";
+	cout << "   1. New Order\n";
+	cout << "   2. View All\n";
+	cout << "   3. Cancel Order\n";
+	cout << "   4. Received Orders\n";
+	cout << "   5. Return To Main Menu\n\n\n";
+	cout << "Enter Your Choice : \n";
+
+    cin >> funcChoice;
+}
+
+void Menus::salesMenu() {
+    int funcChoice;
+
+    cout << "*************************************************\n";
+	cout << "                    Sales List                   \n";
+	cout << "*************************************************\n";
+	cout << "   1. Add New Bill\n";
+	cout << "   2. Total Sales\n";
+	cout << "   3. Return To Main Menu\n\n\n";
+	cout << "Enter Your Choice : \n";
+
+    cin >> funcChoice;
+}
+
+void Menus::suppliersMenu() {
+    int funcChoice;
+
+    cout << "*************************************************\n";
+	cout << "                   Suppliers List                \n";
+	cout << "*************************************************\n";
+	cout << "   1. Add Supplier\n";
+	cout << "   2. Remove Supplier\n";
+	cout << "   3. Search Supplier\n";
+	cout << "   4. Return To Main Menu\n\n\n";
+	cout << "Enter Your Choice : \n";
+
+    cin >> funcChoice;
+}
